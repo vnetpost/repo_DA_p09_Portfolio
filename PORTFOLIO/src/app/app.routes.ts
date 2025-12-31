4210715d824a1ac4612f6bef1c0074dcf1594934
vnetpost <vnetpost@gmail.com>
2025-12-31 14:20:21 +0000

    Added -> styles/ ├─ _variables.scss ├─ _mixins.scss ├─ _fonts.scss └─ index.scss

--- PORTFOLIO/src/app/app.routes.ts
+++ PORTFOLIO/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:MainPage},
+];
