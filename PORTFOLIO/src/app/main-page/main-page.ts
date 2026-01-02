104ba0db2766ddde4941012c63ae8ae79dea02d9
vnetpost <vnetpost@gmail.com>
2026-01-02 09:07:27 +0000

    header-done-without-anim

--- PORTFOLIO/src/app/main-page/main-page.ts
+++ PORTFOLIO/src/app/main-page/main-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [Hero],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
--- PORTFOLIO/src/app/shared/header/header.ts
+++ PORTFOLIO/src/app/shared/header/header.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+type Lang = 'en' | 'de';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  isMenuOpen = false;
+  lang: Lang = 'en';
 
+  setLang(l: Lang) {
+    this.lang = l;
+    console.log(this.lang);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    if (!this.isMenuOpen) return;
+    this.closeMenu();
+  }
 }
