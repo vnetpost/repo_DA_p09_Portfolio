0b19e67fd4abf25cd6ae575971ce0379d06ad72f
vnetpost <vnetpost@gmail.com>
2026-01-02 17:14:57 +0000

    hero-done

--- PORTFOLIO/src/app/app.ts
+++ PORTFOLIO/src/app/app.ts
@@ -5,7 +5,7 @@ import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- PORTFOLIO/src/app/main-page/main-page.ts
+++ PORTFOLIO/src/app/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
+import { AboutMe } from './about-me/about-me';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero],
+  imports: [Hero, AboutMe],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
