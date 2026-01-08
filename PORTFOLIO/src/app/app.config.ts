811dd848a09dbd1966325d2ca78a1495c6f2b18f
vnetpost <vnetpost@gmail.com>
2026-01-08 21:31:46 +0000

    anchor-scrolling-optimized

--- PORTFOLIO/src/app/app.config.ts
+++ PORTFOLIO/src/app/app.config.ts
@@ -1,17 +1,35 @@
 import {
+  APP_INITIALIZER,
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+
+const initScrollOffset = (viewportScroller: ViewportScroller) => () => {
+  viewportScroller.setOffset([0, 85]);
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'top',
+      })
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initScrollOffset,
+      deps: [ViewportScroller],
+      multi: true,
+    },
     provideHttpClient(),
   ],
 };
